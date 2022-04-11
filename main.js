5810a370141d0ac3f089394395b07206c0857fdd
francodml <7284418+francodml@users.noreply.github.com>
2022-04-11 14:18:43 -0300

    Don't use mongoose, push changes through backend

--- main.js
+++ main.js
@@ -1,35 +1,14 @@
 import 'dotenv/config';
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
-import mongoose from 'mongoose';
-
-import User from '../models/user.js';
-import Development from '../models/development.js';
-import Hackathon, {HackathonEntry} from '../models/hackathon.js';
-
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-const DB_NAME = process.env.DB_NAME;
-
-const CONNECTION_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.vql6v.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
-mongoose.connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB');
-    runApp();
-})
-.catch((err) => {
-    console.log(err);
-});
 
 const axinstance = axios.create({
     baseURL: 'http://localhost:3001/',
 });
 
 const args = process.argv.slice(2);
+
+runApp(args);
 
 function generateRandomUsers(count) {
     const users = [];
@@ -65,7 +44,7 @@ function generateRandomHackathon(users) {
     return hackathon;
 }
 
-function runApp() {
+function runApp(args) {
 
 
 
